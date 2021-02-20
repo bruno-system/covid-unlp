7ff158ffa42b366cd014b46f57467d29a915490a
BrunoA <38539134+bruno-system@users.noreply.github.com>
2021-02-20 00:07:32 -0300

    api de ejemplo

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import { StyleSheet, Text, View, Button, Image, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const [currentImageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    // Se ejecuta cuando se monta el componente
+    loadRandomImage();
+  }, []);
+
+  const loadRandomImage = () => {
+    const width = Math.floor(Dimensions.get('window').width);
+    const height = Math.floor(Dimensions.get('window').height);
+    fetch('https://picsum.photos/' + width + '/' + height)
+    .then(async function (response){
+        // guardamos en el state la url de la foto random
+        setImageUrl(response.url);
+
+    });
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={styles.main}>
+      <View style={styles.imageHolder}>
+          {(currentImageUrl != null) && (     
+            //Que valida q se renderisó?         
+            <Image style={styles.image} source={{ uri: currentImageUrl }} />
+          )
+          }                 
+      </View>
+      <View style={styles.footer}>
+          <Button title="Descubrir nueva" onPress={loadRandomImage} />
+          <Button title="Mis favoritas" onPress={() => {}} />
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -12,10 +42,31 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main: {
+      flex: 1,
+      flexDirection: 'column',
   },
+  touchableOpacity: {
+      flex: 1
+  },
+  imageHolder: {
+      flex: 1
+  },
+  image: {
+      flex: 1
+  },
+  favIconTouchableOpacity: {
+      position: 'absolute',
+      right: 20,
+      top: 40
+  },
+  footer: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 8,
+  }
 });
