abe7c11bc6af3fca912325e9931bd024176b302f
BrunoA <38539134+bruno-system@users.noreply.github.com>
2021-03-07 23:30:32 -0300

    probando shearcheable

--- screens/Countries.js
+++ screens/Countries.js
@@ -1,7 +1,8 @@
 import React , {useState, useEffect} from 'react';
-import { Image, StyleSheet, Text, View, ActivityIndicator, FlatList, } from 'react-native';
+import { Image, StyleSheet, Text, View, ActivityIndicator, FlatList,SafeAreaView, StatusBar  } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 export default function Countries({ navigation }) {
     
@@ -10,7 +11,7 @@ export default function Countries({ navigation }) {
 
     useEffect(() => {
         // Se ejecuta cuando se monta el componente
-        loadCountries();
+        loadCountries(); 
     }, []);
 
     var requestOptions = {
@@ -40,13 +41,13 @@ export default function Countries({ navigation }) {
 
         // });
 
-
+        
     };
 
 
 
 
-  return (
+  return ( 
     <View style={styles.container}>
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
 
@@ -57,13 +58,63 @@ export default function Countries({ navigation }) {
 
       {isLoading ? <ActivityIndicator/> : (
 
-        <FlatList
-          data={listaPaises}
-          keyExtractor={item => item.ISO2}
-          renderItem={({ item }) => (
-            <Text>{item.Country}, {item.Slug}</Text>
-          )}
-        />
+
+        <SafeAreaView style={styles.container}>
+
+          {/* <FlatList
+            data={listaPaises}
+            keyExtractor={item => item.ISO2}
+            renderItem={({ item }) => (
+              <Text>{item.Country}, {item.Slug}</Text>
+            )}
+          /> */}
+
+
+            <SearchableDropdown
+              onTextChange={(text) => console.log(text)}
+              //On text change listner on the searchable input
+              onItemSelect={(item) => alert(JSON.stringify(item))}
+              //onItemSelect called after the selection from the dropdown
+              containerStyle={{ padding: 5 }}
+              //suggestion container style
+              textInputStyle={{
+                //inserted text style
+                padding: 12,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                backgroundColor: '#FAF7F6',
+              }}
+              itemStyle={{
+                //single dropdown item style
+                padding: 10,
+                marginTop: 2,
+                backgroundColor: '#FAF9F8',
+                borderColor: '#bbb',
+                borderWidth: 1,
+              }}
+              itemTextStyle={{
+                //text style of a single dropdown item
+                color: '#222',
+              }}
+              itemsContainerStyle={{
+                //items container style you can pass maxHeight
+                //to restrict the items dropdown hieght
+                maxHeight: '60%',
+              }}
+              items={listaPaises}
+              //mapping of item array
+              // defaultIndex={2}
+              //default selected item index
+              placeholder="placeholder"
+              //place holder for the search input
+              resetValue={false}
+              //reset textInput Value with true and false state
+              underlineColorAndroid="transparent"
+              //To remove the underline from the android input
+            />
+
+        </SafeAreaView>
+
 
       )}
 
@@ -78,6 +129,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
   },
   logo: {
     width: 305,
